13d9c10723ecd392b0f89eef922f984ac17d99d3
Enan Bester <ebester@tudelft.nl>
2026-01-12 12:45:18 +0100

    changing graphs to mean and deviation plot

--- results/viz_results.py
+++ results/viz_results.py
@@ -2,19 +2,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# colors = ['tab:cyan', 'tab:purple', 'tab:orange']
+
 data_1 = np.load("results_hallway_v1.npz")['arr_0']
 data_2 = np.load("results_hallway_v2.npz")['arr_0']
 data_3 = np.load("results_hallway_v3.npz")['arr_0']
 
-fig, axs = plt.subplots(1, 1, figsize=(7, 7))
+# fig, axs = plt.subplots(1, 1, figsize=(7, 7))
 
-axs.scatter(data_1[:, 1], 1000*data_1[:, 2], color='tab:cyan', label='MPC 1')
-axs.scatter(data_2[:, 1], 1000*data_2[:, 2], color='tab:red', label='MPC 2')
-axs.scatter(data_3[:, 1], 1000*data_3[:, 2], color='tab:orange', label='MPC 3')
+# axs.scatter(data_1[:, 1], 1000*data_1[:, 2], color='tab:cyan', label='MPC 1')
+# axs.scatter(data_2[:, 1], 1000*data_2[:, 2], color='tab:red', label='MPC 2')
+# axs.scatter(data_3[:, 1], 1000*data_3[:, 2], color='tab:orange', label='MPC 3')
 
-axs.set_title('Hallway Results')
-axs.set_xlabel("Average velocity [m/s]")
-axs.set_ylabel("RMS Error [mm]")
 
-axs.legend()
+fig, ax = plt.subplots(figsize=(7, 6))
+
+
+ax.errorbar(
+    np.mean(data_1[:, 1]), np.mean(1000*data_1[:, 2]),
+    xerr=np.std(data_1[:, 1]),
+    yerr=np.std(1000*data_1[:, 2]),
+    fmt='o',
+    capsize=6,
+    markersize=8,
+    color='tab:cyan',
+    label='MPC 1'
+)
+
+ax.errorbar(
+    np.mean(data_2[:, 1]), np.mean(1000*data_2[:, 2]),
+    xerr=np.std(data_2[:, 1]),
+    yerr=np.std(1000*data_2[:, 2]),
+    fmt='o',
+    capsize=6,
+    markersize=8,
+    color='tab:purple',
+    label='MPC 2'
+)
+
+ax.errorbar(
+    np.mean(data_3[:, 1]), np.mean(1000*data_3[:, 2]),
+    xerr=np.std(data_3[:, 1]),
+    yerr=np.std(1000*data_3[:, 2]),
+    fmt='o',
+    capsize=6,
+    markersize=8,
+    color='tab:orange',
+    label='MPC 3'
+)
+
+
+
+ax.set_title('MPC comparison')
+ax.set_xlabel("Average velocity [m/s]")
+ax.set_ylabel("RMS Error [mm]")
+ax.grid(True, linestyle='--', alpha=0.4)
+ax.legend()
+
+plt.tight_layout()
 plt.show()
+
